946a36590fbc45eafcfd21269c78f252a05fe25d
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-28 14:13:00 -0300

    delete

--- curso/src/main/java/com/remedios/curso/controller/remedioController.java
+++ curso/src/main/java/com/remedios/curso/controller/remedioController.java
@@ -3,7 +3,9 @@ package com.remedios.curso.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,5 +43,11 @@ public class remedioController {
     public void atualizar(@RequestBody @Valid DadoaAtualizarRemedio dados) {
         var remedio = repository.getReferenceById(dados.id());
         remedio.atuaInformacoes(dados);
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
     }
 }
