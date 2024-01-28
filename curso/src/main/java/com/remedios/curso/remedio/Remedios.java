50f9f1a883d76d58406c438ff3e4fb8c8d9e6173
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-28 09:20:45 -0300

    migrations com flyway

--- curso/src/main/java/com/remedios/curso/remedio/Remedios.java
+++ curso/src/main/java/com/remedios/curso/remedio/Remedios.java
@@ -13,8 +13,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Table(name = "remedios")
 @Entity(name = "remedios")
-@Table(name = "Remedios")
 @Getter
 @Setter
 @AllArgsConstructor
