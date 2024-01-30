0eff521a34130c9d0213d9b0151ee6b8042433d6
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-30 14:25:30 -0300

    erro 404

---
+++ curso/src/main/java/com/remedios/curso/infra/TratadorDeErros.java
@@ -0,0 +1,14 @@
+package com.remedios.curso.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+@RestControllerAdvice
+public class TratadorDeErros {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> tratador404() {
+        return ResponseEntity.notFound().build();
+    }
+}
