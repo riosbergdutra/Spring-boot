aacb5c55c08b574ecc6b1b307e9364478886e9b9
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-31 10:27:54 -0300

    correcao

--- curso/src/main/java/com/remedios/curso/infra/securityConfiguration.java
+++ curso/src/main/java/com/remedios/curso/infra/securityConfiguration.java
@@ -1,20 +1,15 @@
 package com.remedios.curso.infra;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class securityConfiguration {
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
-    }
+class SecurityConfigurations {
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+      .csrf(Customizer.withDefaults())
+      .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .build();
+  }
 }
