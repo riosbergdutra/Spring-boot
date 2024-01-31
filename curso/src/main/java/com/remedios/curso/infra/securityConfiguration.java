fe0f953fae3a97008ad2384273be8096686226f3
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-31 10:37:45 -0300

    Fix: desabilitando o csrf

--- curso/src/main/java/com/remedios/curso/infra/securityConfiguration.java
+++ curso/src/main/java/com/remedios/curso/infra/securityConfiguration.java
@@ -1,15 +1,22 @@
 package com.remedios.curso.infra;
 
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-class SecurityConfigurations {
+@Configuration
+@EnableWebSecurity
+class securityConfiguration {
+
+@Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http
-      .csrf(Customizer.withDefaults())
+      .csrf(custom -> custom.disable())
       .sessionManagement(customizer -> customizer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .build();
   }
 }
+
