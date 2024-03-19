c6a7d78897ca31250c269706eb5c787cbd16441b
Riosberg Dutra <riosbergduttra@gmail.com>
2024-03-19 06:27:50 -0300

    arquivo base

---
+++ event-driven/src/main/java/test/event/eventdriven/EventDrivenApplication.java
@@ -0,0 +1,14 @@
+package test.event.eventdriven;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EventDrivenApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDrivenApplication.class, args);
+	}
+}
