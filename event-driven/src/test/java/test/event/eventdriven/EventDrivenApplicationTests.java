5fef6cbe699c4454c78650e96c46367b5852f16f
Riosberg Dutra <riosbergduttra@gmail.com>
2024-03-19 06:24:54 -0300

    arquivo de teste gerado

---
+++ event-driven/src/test/java/test/event/eventdriven/EventDrivenApplicationTests.java
@@ -0,0 +1,13 @@
+package test.event.eventdriven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
