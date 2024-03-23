d9a591cca0ee3c598e0c6f99e01f88c2e12f8958
Riosberg Dutra <riosbergduttra@gmail.com>
2024-03-23 17:55:43 -0300

    ainda fazer arquivos de testes

---
+++ lambda/src/test/java/lambda/unitarios/lambda/LambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package lambda.unitarios.lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
