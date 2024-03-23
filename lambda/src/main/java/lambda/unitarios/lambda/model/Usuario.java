3eceaa084fb76153354740ccdf61600f2fdfad0e
Riosberg Dutra <riosbergduttra@gmail.com>
2024-03-23 17:56:30 -0300

    model de usuario

---
+++ lambda/src/main/java/lambda/unitarios/lambda/model/Usuario.java
@@ -0,0 +1,33 @@
+package lambda.unitarios.lambda.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuariotest")
+public class Usuario {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+}
